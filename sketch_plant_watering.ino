74c7981da4cd276fc510a6638586ba88157948e5
SudoCode7 <63919871+SudoCode7@users.noreply.github.com>
2022-07-28 21:32:48 +0530

    Update sketch_plant_watering.ino

--- sketch_plant_watering.ino
+++ sketch_plant_watering.ino
@@ -1,6 +1,6 @@
 #include<Narcoleptic.h>
 const byte l=0;
-byte d=l, n=8;//pin number plus 2 &&  n= number of plants
+byte d=l, n=8; //n represents number of plants
 void setup()
 {
  for(byte i=0;i<n;i++)
@@ -14,7 +14,7 @@ void loop()
 {  
   for(byte i=0;i<n;i++)
   {
-   if(d==2||d==3||d==4)// can be added n numbers of if else according to water requirements with delay times
+   if(d==2||d==3||d==4) // n number of motors can be added according to water requirements with delay time to trigger the motor for the time limit
     {
       digitalWrite(d,HIGH);
       Narcoleptic.delay(4000);
@@ -23,6 +23,6 @@ void loop()
   d++;
   }
   
- Narcoleptic.delay(86400000);// for 1 day delay
+ Narcoleptic.delay(86400000); // for 1 day delay (in seconds)
 
 }
